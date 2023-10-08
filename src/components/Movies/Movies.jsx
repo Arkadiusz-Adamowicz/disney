aeaa52fbdb8418e24cc24ab0700f00413de790bd
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2023-10-08 17:37:09 +0200

    fix paddings

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -12,7 +12,7 @@ const Movies = () => {
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>
             Now Playing
           </h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {movies.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
@@ -24,7 +24,7 @@ const Movies = () => {
       <div className='text-white flex flex-col w-full mb-[-20px] gap-5 m-auto'>
         <div className='flex flex-col gap-5 mt-4'>
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>Top Rated</h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {topRated.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
@@ -36,7 +36,7 @@ const Movies = () => {
       <div className='text-white flex flex-col w-full mb-[-20px] gap-5 m-auto'>
         <div className='flex flex-col gap-5 mt-4'>
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>Trending</h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {trending.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
@@ -48,7 +48,7 @@ const Movies = () => {
       <div className='text-white flex flex-col w-full mb-[-20px] gap-5 m-auto'>
         <div className='flex flex-col gap-5 mt-4'>
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>Action</h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {action.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
@@ -60,7 +60,7 @@ const Movies = () => {
       <div className='text-white flex flex-col w-full mb-[-20px] gap-5 m-auto'>
         <div className='flex flex-col gap-5 mt-4'>
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>Comedy</h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {comedy.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
@@ -74,7 +74,7 @@ const Movies = () => {
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>
             Documentary
           </h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {documentary.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
@@ -86,7 +86,7 @@ const Movies = () => {
       <div className='text-white flex flex-col w-full mb-[-20px] gap-5 m-auto'>
         <div className='flex flex-col gap-5 mt-4'>
           <h2 className='text-15px md:text-[22px] px-1 font-bold'>TV</h2>
-          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-5'>
+          <div className='flex overflow-x-auto px-1 pt-1 pb-10 gap-2 md:gap-5'>
             {tv.map(movie => (
               <div key={movie.id}>
                 <Movie movie={movie} />
