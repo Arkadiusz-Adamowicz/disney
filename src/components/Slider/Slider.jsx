00984b19e547556e4f482ca46e53372e0ed6b719
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2023-10-14 19:34:01 +0200

    fix slider paddings

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -5,13 +5,14 @@ const Slider = () => {
   const { movies } = useMovies();
   const image_base_url = 'https://image.tmdb.org/t/p/original';
   return (
-    <div className='mx-6 md:mx-16 mt-2'>
-      <div className='text-white items-center mx-auto mb-4  '>
+    <div className='mx-5 md:mx-16 mt-2'>
+      <div className='text-white items-center mx-auto mb-4 '>
         <Carousel
+          navigation={false}
           autoplay
           loop
           transition={{ duration: 1 }}
-          className='rounded-lg shadow-lg shadow-black border-[2px] border-gray-600 '
+          className='rounded-lg shadow-lg shadow-black border-[2px] border-gray-600'
         >
           {movies
             .sort((a, b) => (a.title < b.title ? -1 : 1))
@@ -19,7 +20,7 @@ const Slider = () => {
               <div key={movie.id}>
                 <img
                   src={image_base_url + movie.backdrop_path}
-                  className='w-full h-[200px] md:h-[420px] object-cover object-top rounded-lg '
+                  className='w-full h-[200px] md:h-[420px] object-cover object-top'
                 />
               </div>
             ))}
