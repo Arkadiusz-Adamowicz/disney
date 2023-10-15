5bce2a64c2ad60cf03325de487af800045c26d82
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2023-10-15 09:57:07 +0200

    fix paddings

--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -1,12 +1,12 @@
 const Movie = ({ movie }) => {
-  const image_base_url = "https://image.tmdb.org/t/p/original";
+  const image_base_url = 'https://image.tmdb.org/t/p/original';
   return (
-    <div className="text-white rounded-lg w-[100px] md:w-[200px] shadow-lg shadow-black border-[2px] border-gray-600 hover:border-white hover:scale-[102%] transition-all duration-300 ease-in-out cursor-pointer">
-      <div className="h-[150px] md:h-[300px]">
+    <div className='text-white rounded-lg w-[100px] md:w-[200px] shadow-lg shadow-black border-[2px] border-gray-600 transition-all duration-300 ease-in-out cursor-pointer'>
+      <div className='h-[150px] md:h-[300px]'>
         <img
           src={image_base_url + movie.poster_path}
-          alt="poster"
-          className="rounded-md h-full "
+          alt='poster'
+          className='rounded-md h-full '
         />
       </div>
     </div>
--- src/components/ProductionHouse/ProductionHouse.jsx
+++ src/components/ProductionHouse/ProductionHouse.jsx
@@ -1,14 +1,14 @@
-import disney from "../../assets/Images/disney.png";
-import marvel from "../../assets/Images/marvel.png";
-import nationalG from "../../assets/Images/nationalG.png";
-import pixar from "../../assets/Images/pixar.png";
-import starwars from "../../assets/Images/starwars.png";
+import disney from '../../assets/Images/disney.png';
+import marvel from '../../assets/Images/marvel.png';
+import nationalG from '../../assets/Images/nationalG.png';
+import pixar from '../../assets/Images/pixar.png';
+import starwars from '../../assets/Images/starwars.png';
 
-import disneyVid from "../../assets/Videos/disney.mp4";
-import marvelVid from "../../assets/Videos/marvel.mp4";
-import nationalGVid from "../../assets/Videos/national.mp4";
-import pixarVid from "../../assets/Videos/pixar.mp4";
-import starwarsVid from "../../assets/Videos/star-wars.mp4";
+import disneyVid from '../../assets/Videos/disney.mp4';
+import marvelVid from '../../assets/Videos/marvel.mp4';
+import nationalGVid from '../../assets/Videos/national.mp4';
+import pixarVid from '../../assets/Videos/pixar.mp4';
+import starwarsVid from '../../assets/Videos/star-wars.mp4';
 
 const ProductionHouse = () => {
   const productionHouseList = [
@@ -40,13 +40,13 @@ const ProductionHouse = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center gap-2 md:gap-6 p-2 px-5 md:px-16">
-      {productionHouseList.map((item) => (
+    <div className='flex items-center justify-center gap-2 md:gap-6 p-2 px-5 md:px-16'>
+      {productionHouseList.map(item => (
         <div
           key={item.id}
-          className="border-[2px] border-gray-600 hover:border-white rounded-lg shadow-black shadow-lg relative hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer"
+          className='border-[2px] border-gray-600  rounded-lg shadow-black shadow-lg relative transition-all duration-300 ease-in-out cursor-pointer'
         >
-          <img src={item.image} alt={item.name} className="absolute" />
+          <img src={item.image} alt={item.name} className='absolute' />
           <video
             src={item.video}
             alt={item.name}
@@ -54,7 +54,7 @@ const ProductionHouse = () => {
             loop
             muted
             playsInline
-            className="rounded-md opacity-0 hover:opacity-100"
+            className='rounded-md opacity-0 hover:opacity-100'
           />
         </div>
       ))}
